31ffb2f34ec8a2538469d2dcc6823689991a187c
F. Stephen Quaratiello <fstephen@mercuryq.net>
2013-04-07 23:24:47 -0400

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   php/loginform.php

---
+++ php/loginform.php
@@ -0,0 +1,18 @@
+<?php
+class loginform{
+	function foo(){
+
+		if($_SESSION['numtries'] >= 3){
+			echo "You have exceeded your login tries. Please come back later";
+		}
+		else{
+			echo $_SESSION['flash_error'];
+			$_SESSION['flash_error'] = null;
+echo '<form method="post" name="form1" action="script/login.php">';
+echo '<input type="text" name="username" /></br>';
+echo '<input type="password"  name="password" />';
+echo '<input type="submit" value="Sign in" />';
+		}
+	}
+}
+?>
